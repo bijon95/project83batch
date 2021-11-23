1b6dd158462e9dfb47e9358131695394ea54da61
bijon95 <bijoninfo@gmail.com>
2021-11-23 12:15:32 +0600

    login page api connect

--- lib/Auth/login.dart
+++ lib/Auth/login.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:project83batch/homepage.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -11,15 +14,34 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController phone_ct = TextEditingController();
   TextEditingController pass_ct = TextEditingController();
-
+var msg ="";
   usereLogin()async{
-    String url = "";
+    String url = "https://api.ishaqi.com/customerlogin";
     Uri myuri = Uri.parse(url);
     var client = http.Client();
-    var responce = client.post(myuri,body: );
+    Map logindetails = {
+      "user_nam": phone_ct.text,
+      "password": pass_ct.text,
+    };
+    var body = jsonEncode(logindetails);
+
+    var responce = await client.post(myuri,body: body);
 
 
-    print(responce);
+    print(responce.statusCode);
+    if(responce.statusCode==200
+    ){
+      var data = jsonDecode(responce.body);
+      msg = data["message"];
+      if(data["status"]=="SUCCESS"){
+        
+        Navigator.push(context, MaterialPageRoute(builder: (builder)=> HomePage()));
+      }
+      setState(() {
+
+      });
+    }
+    print(responce.body);
 
   }
 
@@ -42,16 +64,23 @@ class _LoginState extends State<Login> {
 
             ),
 
-            Container(
-              margin: EdgeInsets.only(top: 30),
-              height: 60,
+            Text(msg),
 
-              width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(15)
-                ),
-              child: Center(child: Text("Login",style: TextStyle(fontSize: 24,color: Colors.white),)),
+            InkWell(
+              onTap: (){
+                usereLogin();
+              },
+              child: Container(
+                margin: EdgeInsets.only(top: 30),
+                height: 60,
+
+                width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                child: Center(child: Text("Login",style: TextStyle(fontSize: 24,color: Colors.white),)),
+              ),
             )
           ],
         ));
