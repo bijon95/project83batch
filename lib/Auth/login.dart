0ba07e2b2e11144dd11eeada4e94d7a25bc12e14
bijon95 <bijoninfo@gmail.com>
2021-11-23 11:41:17 +0600

    login page ui

--- lib/Auth/login.dart
+++ lib/Auth/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -10,12 +11,30 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Container(
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Enter your Mobile no and password"),
+            TextField(
 
-      )
-    );
+            ),
+            TextField(),
+
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              height: 60,
+
+              width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(15)
+                ),
+              child: Center(child: Text("Login",style: TextStyle(fontSize: 24,color: Colors.white),)),
+            )
+          ],
+        ));
   }
 }
