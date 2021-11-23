6bcaac475524dec086ea55493282b0319027b7f1
rabeya95 <rimiruhiyousuf@gmail.com>
2021-11-23 11:17:34 +0600

    Homepage Edit

--- lib/Auth/login.dart
+++ lib/Auth/login.dart
@@ -1,41 +1,21 @@
 import 'package:flutter/material.dart';
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _LoginState createState() => _LoginState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Login v")),
+        title: Text("Login"),
       ),
       body: Container(
-        color: Colors.orangeAccent,
-        child: Column(
-          children: [
-            Text("username", style: TextStyle(color: Colors.black.withOpacity(0.8)),),
-            TextField(),
-            Text("Password", style: TextStyle(color: Colors.black.withOpacity(0.8)),),
-            TextField(),
 
-            TextButton(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              ),
-              onPressed: () { },
-              child: Text('Login', style: TextStyle(color: Colors.black.withOpacity(0.8)),),
-            )
-          ],
-
-
-        ),
-
-      ),
-
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'Auth/login.dart';
+import 'homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
